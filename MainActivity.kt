89ac0475fa8f2981c2a6db206eaea1c86fb7ad2a
João Freitas <136653942+joaodfreitas@users.noreply.github.com>
2024-09-01 18:35:11 -0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.verificadorsenha
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Referências aos elementos da interface
+        val editTextSenha = findViewById<EditText>(R.id.editTextSenha)
+        val buttonVerificarSenha = findViewById<Button>(R.id.buttonVerificarSenha)
+        val textViewResultado = findViewById<TextView>(R.id.textViewResultado)
+
+        // Senha fixa
+        val senhaCorreta = "1234"
+
+        buttonVerificarSenha.setOnClickListener {
+            val senhaInserida = editTextSenha.text.toString()
+
+            if (senhaInserida == senhaCorreta) {
+                textViewResultado.text = "Senha correta"
+            } else {
+                textViewResultado.text = "Senha incorreta"
+            }
+        }
+    }
+}
